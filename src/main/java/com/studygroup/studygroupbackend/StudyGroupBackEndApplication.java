3d70662bc2e727194fb20e8a7a420b8a56395632
hyukjin0419 <21900771@handong.ac.kr>
2025-03-12 18:42:54 +0900

    Initial commit

---
+++ src/main/java/com/studygroup/studygroupbackend/StudyGroupBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.studygroup.studygroupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGroupBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGroupBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studygroup/studygroupbackend/StudyGroupBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studygroup.studygroupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGroupBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
