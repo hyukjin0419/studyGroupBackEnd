5abd9f3e4415bc4efe2193d9621d34c2106c3aa5
hyukjin0419 <21900771@handong.ac.kr>
2025-03-14 00:51:06 +0900

    feature/#4/enitty-setup-upgrade/MemberDto.java upgrade

--- src/main/java/com/studygroup/studygroupbackend/dto/MemberDto.java
+++ src/main/java/com/studygroup/studygroupbackend/dto/MemberDto.java
@@ -1,22 +1,66 @@
 package com.studygroup.studygroupbackend.dto;
 
 import com.studygroup.studygroupbackend.entity.Member;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Builder
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class MemberDto {
 
-    private Long id;
-    private String userName;
-    private String email;
+    // 회원 가입 요청
+    @Getter
+    @Builder
+    public static class CreateReqDto {
+        private final String userName;
+        private final String password;
+        private final String email;
 
-    public static MemberDto formEntity(Member member) {
-        return MemberDto.builder
+        public Member toEntity(){
+            return Member.of(userName, password, email);
+        }
     }
+
+    //로그인 요청
+    @Getter
+    @Builder
+    public static class LoginReqDto {
+        private final String userName;
+        private final String password;
+    }
+
+    //회원 상세 조회
+    @Getter
+    @Builder
+    public static class DetailResDto {
+        private final Long id;
+        private final String userName;
+        private final String email;
+
+        public static DetailResDto fromEntity(Member member) {
+            return DetailResDto.builder()
+                    .id(member.getId())
+                    .userName(member.getUserName())
+                    .email(member.getEmail())
+                    .build();
+        }
+    }
+
+    //회원 목록 조회 응답 DTO
+    @Getter
+    @Builder
+    public static class ListResDto{
+        private final Long id;
+        private final String userName;
+        private final String email;
+
+        public static ListResDto fromEntity(Member member) {
+            return ListResDto.builder()
+                    .id(member.getId())
+                    .userName(member.getUserName())
+                    .email(member.getEmail())
+                    .build();
+        }
+    }
+
+
+
 }
