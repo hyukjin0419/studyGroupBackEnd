47ee12e30f56eabb539c23295c38c4fd8c365b59
hyukjin0419 <21900771@handong.ac.kr>
2025-04-01 17:56:42 +0900

    feat/#13/checklist-develop/ChecklistService & ChecklistServiceImpl

--- src/main/java/com/studygroup/studygroupbackend/service/ChecklistService.java
+++ src/main/java/com/studygroup/studygroupbackend/service/ChecklistService.java
@@ -1,2 +1,11 @@
-package com.studygroup.studygroupbackend.service;public interface ChecklistService {
+package com.studygroup.studygroupbackend.service;
+
+import com.studygroup.studygroupbackend.dto.ChecklistDto;
+
+public interface ChecklistService {
+    public ChecklistDto.CreateResDto createChecklist(Long createdId, ChecklistDto.CreateReqDto request);
+    public void updateContent(Long checklistId, String content);
+    public void updateDueDate(Long checklistId, java.time.LocalDateTime dueDate);
+    public ChecklistDto.DetailResDto getChecklistDetail(Long checklistId) ;
+    public ChecklistDto.DeleteResDto deleteResDto(Long checklistId);
 }
