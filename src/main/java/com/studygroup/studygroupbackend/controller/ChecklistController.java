56a6460475c53a61c45be3b963e5a809ad23ef81
hyukjin0419 <21900771@handong.ac.kr>
2025-04-02 00:06:17 +0900

    chore/#23/swagger-API/ChecklistController

--- src/main/java/com/studygroup/studygroupbackend/controller/ChecklistController.java
+++ src/main/java/com/studygroup/studygroupbackend/controller/ChecklistController.java
@@ -2,6 +2,7 @@ package com.studygroup.studygroupbackend.controller;
 
 import com.studygroup.studygroupbackend.dto.ChecklistDto;
 import com.studygroup.studygroupbackend.service.ChecklistService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ public class ChecklistController {
 
     private final ChecklistService checklistService;
 
+    @Operation(summary = "체크리스트 생성 API")
     @PostMapping
     public ResponseEntity<ChecklistDto.CreateResDto> createChecklist(
             @RequestHeader("X-Member-Id") Long creatorId,
@@ -20,6 +22,7 @@ public class ChecklistController {
         return ResponseEntity.ok(checklistService.createChecklist(creatorId, request));
     }
 
+    @Operation(summary = "체크리스트 컨텐츠 업데이트 API")
     @PatchMapping("/{id}/content")
     public ResponseEntity<Void> updateContent(
             @PathVariable Long id,
@@ -28,6 +31,7 @@ public class ChecklistController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "체크리스트 날짜 업데이트 API")
     @PatchMapping("/{id}/duedate")
     public ResponseEntity<Void> updateDueDate(
             @PathVariable Long id,
@@ -36,11 +40,13 @@ public class ChecklistController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "체크리스트 단일 조회 API")
     @GetMapping("/{id}")
     public ResponseEntity<ChecklistDto.DetailResDto> getChecklistDetail(@PathVariable Long id) {
         return ResponseEntity.ok(checklistService.getChecklistDetail(id));
     }
 
+    @Operation(summary = "체크리스트 삭제 API")
     @DeleteMapping("{id}")
     public ResponseEntity<ChecklistDto.DeleteResDto> deleteChecklist(@PathVariable Long id) {
         return ResponseEntity.ok(checklistService.deleteChecklist(id));
