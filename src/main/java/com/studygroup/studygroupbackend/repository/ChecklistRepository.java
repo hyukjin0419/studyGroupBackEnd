9263881b09e9e2ea9da70c3888fdc34cd26ea6b9
hyukjin0419 <21900771@handong.ac.kr>
2025-04-01 17:54:47 +0900

    feat/#13/checklist-develop/ChecklistService & ChecklistServiceImpl

---
+++ src/main/java/com/studygroup/studygroupbackend/repository/ChecklistRepository.java
@@ -0,0 +1,2 @@
+package com.studygroup.studygroupbackend.repository;public class ChecklistRepository {
+}
---
+++ src/main/java/com/studygroup/studygroupbackend/service/ChecklistService.java
@@ -0,0 +1,2 @@
+package com.studygroup.studygroupbackend.service;public interface ChecklistService {
+}
