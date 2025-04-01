a9eb82a32c3db42827a3fc96aa405eac91f110e2
hyukjin0419 <21900771@handong.ac.kr>
2025-04-02 00:07:54 +0900

    chore/#23/swagger-API/ChecklistMemberController

--- src/main/java/com/studygroup/studygroupbackend/controller/ChecklistMemberController.java
+++ src/main/java/com/studygroup/studygroupbackend/controller/ChecklistMemberController.java
@@ -2,6 +2,7 @@ package com.studygroup.studygroupbackend.controller;
 
 import com.studygroup.studygroupbackend.dto.ChecklistMemberDto;
 import com.studygroup.studygroupbackend.service.ChecklistMemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,18 +16,21 @@ public class ChecklistMemberController {
 
     private final ChecklistMemberService checklistMemberService;
 
+    @Operation(summary = "체크리스트 할당 API")
     @PostMapping("/assign")
     public ResponseEntity<ChecklistMemberDto.AssignResDto> assignChecklistToMember(
             @RequestBody ChecklistMemberDto.AssignReqDto request) {
         return ResponseEntity.ok(checklistMemberService.assignChecklist(request));
     }
 
+    @Operation(summary = "체크리스트 체크/언체크 API")
     @PatchMapping("/change-status")
     public ResponseEntity<ChecklistMemberDto.ChangeStatusResDto> changeStatus(
             @RequestBody ChecklistMemberDto.ChangeStatusReqDto request) {
         return ResponseEntity.ok(checklistMemberService.changeStatus(request));
     }
 
+    @Operation(summary = "체크리스트 할당 해제 API")
     @DeleteMapping("/{checklistId}/members/{memberId}")
     public ResponseEntity<ChecklistMemberDto.UnassignResDto> unassignChecklist(
             @PathVariable Long checklistId,
@@ -35,6 +39,7 @@ public class ChecklistMemberController {
         return ResponseEntity.ok(checklistMemberService.unassignChecklist(checklistId, memberId));
     }
 
+    @Operation(summary = "체크리스트 멤버 아이디로 전체 조회 API")
     @GetMapping("/member/{memberId}")
     public ResponseEntity<List<ChecklistMemberDto.MemberChecklistResDto>> getChecklistMember(
             @PathVariable Long memberId
@@ -42,6 +47,7 @@ public class ChecklistMemberController {
         return ResponseEntity.ok(checklistMemberService.getMemberChecklists(memberId));
     }
 
+    @Operation(summary = "체크리스트 스터디 아이디로 전체 조회 API")
     @GetMapping("/study/{studyId}")
     public ResponseEntity<List<ChecklistMemberDto.StudyChecklistMemberResDto>> getChecklistByStudy(
             @PathVariable Long studyId
