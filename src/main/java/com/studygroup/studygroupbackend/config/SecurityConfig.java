307486a848afa487e39f575bfcb2f7a366257c27
hyukjin0419 <21900771@handong.ac.kr>
2025-06-11 11:23:01 +0900

    JwtTokenProvider 기본 설정 생성

--- src/main/java/com/studygroup/studygroupbackend/config/SecurityConfig.java
+++ src/main/java/com/studygroup/studygroupbackend/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.studygroup.studygroupbackend.config;
 
+import com.studygroup.studygroupbackend.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/studygroup/studygroupbackend/jwt/JwtTokenProvider.java
@@ -0,0 +1,94 @@
+package com.studygroup.studygroupbackend.jwt;
+
+import com.studygroup.studygroupbackend.entity.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    @Value("${jwt.secrete}")
+    private String secretKeyString;
+
+    private Key secreteKey;
+
+    private final long accessTokenValidTime = 1000L * 60 * 30; //30분
+    private final long refreshTokenValidTime = 1000L * 60 * 60 * 24 * 7;//7일
+
+    @PostConstruct
+    protected void init() {
+        this.secreteKey = Keys.hmacShaKeyFor(secretKeyString.getBytes());
+    }
+
+    public String generateAccessToken(String userName, Role role) {
+        Claims claims = Jwts.claims().setSubject(userName);
+        claims.put("role",role.name());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessTokenValidTime))
+                .signWith(secreteKey, SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    public String generateRefreshToken(String userName) {
+        return Jwts.builder()
+                .setSubject(userName)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenValidTime))
+                .signWith(secreteKey, SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(secreteKey).build().parseClaimsJwt(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public String getUserName (String token) {
+        return Jwts.parserBuilder().setSigningKey(secreteKey).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public String getRole(String token) {
+        return (String) Jwts.parserBuilder().setSigningKey(secreteKey).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("role");
+    }
+
+    public Authentication getAuthentication(String token) {
+        String username = getUserName(token);
+        String role = getRole(token);
+        User userDetails = new User(username, "", List.of(() -> "ROLE_"+role));
+
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("Authorization");
+    }
+}
