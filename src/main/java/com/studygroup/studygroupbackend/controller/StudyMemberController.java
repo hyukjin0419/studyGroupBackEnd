82def21316d44a3c730168e86f916109e9997dc0
hyukjin0419 <21900771@handong.ac.kr>
2025-07-17 21:32:46 +0900

    member 초대 고민 주석...

--- src/main/java/com/studygroup/studygroupbackend/controller/StudyMemberController.java
+++ src/main/java/com/studygroup/studygroupbackend/controller/StudyMemberController.java
@@ -19,6 +19,41 @@ import org.springframework.web.bind.annotation.*;
 public class StudyMemberController {
     private final StudyMemberService studyMemberService;
 
+    /*
+    멤버 초대하는 시나리오를 어떻게 할까?
+
+    1. 리더가 아이디를 통해서 멤버 초대
+    2. 푸시 알림 가고
+    3. 멤버가 수락 하면
+    4. 초대 완료
+
+    ------------------------------
+    어떻게 구현?
+    - 일단 New Table 2개 필요
+
+    1. StudyInvite 테이블
+    - id
+    - study (FK)
+    - inviter (FK, Member)
+    - invitee (FK, Member)
+    - InviteStatus status (PENDING, ACCEPTED, REJECTED)
+
+--->>>> 여기서 부터 알림에 대해 다시 고민해보쟈
+    2. Notification
+    - id
+    - recipient (FK, Member) 알림을 받을 사용자
+    - Notification type
+    - referencedId...?
+    - message
+
+    public enum NotificationType {
+    INVITE,
+    CHECKLIST_ASSIGNED,
+    CHECKLIST_COMPLETED,
+    STUDY_UPDATED,
+    SYSTEM_MESSAGE
+}
+     */
     @Operation(summary = "스터디에 멤버 초청 API", description = "스터디에 멤버를 초청합니다.")
     @PostMapping
     public ResponseEntity<StudyMemberInviteResponse> inviteMember(
